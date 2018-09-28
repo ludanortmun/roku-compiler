23dac4048f5a02499fcf1e7a05cf237540d5bcf1
Daniel Ortega <danort.97@gmail.com>
2018-09-27 19:15:52 -0700

    create class structure

---
+++ src/lexical/tokenizer.py
@@ -0,0 +1,15 @@
+class LexicalAnalyser():
+    def __init__(self):
+        self.symbol_table = []
+
+    def parse_input(self, source_code):
+        pass
+
+    def add_to_symbol_table(self, token):
+        pass
+    
+    def check_symbol(self, string):
+        pass
+
+    def load_categories(self, path):
+        pass
