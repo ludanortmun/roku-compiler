415b9d503613fe2b00393df6e2494f24c31821f5
Erick Durán <eodnerick@gmail.com>
2018-10-03 10:30:13 -0700

    minor update main

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from src.lexical.lexical_analyser import LexicalAnalyser
 def main():
 	analyser = LexicalAnalyser()
 	analyser.load_categories('src/lexical/lexical_categories.json')
-	
-	tuples = analyser.parse_input('test_source.fang')
+	tuples = analyser.parse_input('tests/test_source.fang')
 
+	# just for dev
 	for i, element in enumerate(tuples):
 		if isinstance(element, tuple):
 			print(str(i) + ' token: ' + element[0] + ' value: ' + element[1])
