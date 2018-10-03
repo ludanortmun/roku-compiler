fda0d3d8bedc53d5e32f3e987c0578410d590afc
Erick Durán <eodnerick@gmail.com>
2018-10-03 10:28:29 -0700

    code cleanup

--- src/lexical/lexical_analyser.py
+++ src/lexical/lexical_analyser.py
@@ -7,84 +7,77 @@ class LexicalAnalyser:
         self.__tuples = []
         self.__categories = []
 
-
     def load_categories(self, path):
         file = open(path, 'r')
         self.__categories = json.load(file)
         pass
-        
 
     def parse_input(self, source_code):
         file = open(source_code, 'r').read()
 
         current_string = ''
         last_correct_string = ''
-        last_correct_category = ''
+        last_correct_category = None
 
         i = 0
+        line_number = 1
         file_length = len(file)
 
         while i < file_length:
             char = file[i]
             current_string += char
-            # print('analysing: ' + self.current_string)
 
             found_category = self.__check_symbol(current_string)
             if found_category is not None:
                 last_correct_string = current_string
                 last_correct_category = found_category
                 i += 1
-
-                # if i == file_length:
-                #     # eof
-                #     print("'" + current_string + "'" + ' is ' + found_category['token'])
             else:
-                if last_correct_category != '':
-                    # print("'" + last_correct_string + "'" + ' is ' + last_correct_category['token'])
-                    if last_correct_category['unique']:
+                if last_correct_category is not None:
+                    if last_correct_category['is_unique']:
                         self.__add_tuple(token=last_correct_category['token'])
                     else:
                         self.__add_tuple(token=last_correct_category['token'], value=last_correct_string)
 
                     current_string = ''
                     last_correct_string = ''
-                    last_correct_category = ''
+                    last_correct_category = None
                 else:
-                    # just for dev
-                    # if current_string == ' ':
-                    #     print ('space found')
-                    # elif current_string == '\t':
-                    #     print ('tab found')
-                    # elif current_string == '\n':
-                    #     print ('tab found')
-                    # else:
-                    #     print("'" + current_string + "'" + ' was not found in categories')
+                    is_space = False
+                    match_result = re.match(self.__categories['spaces']['regex'], current_string)
+
+                    if match_result is not None:
+                        if match_result.start() == 0 and match_result.end() == len(current_string):
+                            if current_string == '\n':
+                                line_number += 1
+                            is_space = True
+
+                    if not is_space:
+                        print('lex error in line %d: character not identified: \'%s\'' % (line_number, current_string))
 
                     current_string = ''
                     last_correct_string = ''
-                    last_correct_category = ''
+                    last_correct_category = None
                     i += 1
 
         return self.__tuples
-
 
     def __add_tuple(self, token, value=None):
         if value:
             self.__tuples.append((token, value))
         else:
-            self.__tuples.append((token))
+            self.__tuples.append(token)
         pass
 
-
-    def __check_symbol(self, string):
+    def __check_symbol(self, symbol):
         for category in self.__categories['categories']:
-            match_result = re.match(category['regex'], string)
+            match_result = re.match(category['regex'], symbol)
 
             if match_result is not None:
-                if match_result.start() == 0 and match_result.end() == len(string):
+                if match_result.start() == 0 and match_result.end() == len(symbol):
                     if 'action' in category:
                         if category['action'] == 'check_value':
-                            is_word_result = self.__check_in_words(string)
+                            is_word_result = self.__check_in_words(symbol)
                             if is_word_result is not None:
                                 return is_word_result
                             else:
@@ -93,11 +86,10 @@ class LexicalAnalyser:
                         return category
         return None
 
-
-    def __check_in_words(self, string):
+    def __check_in_words(self, symbol):
         for word in self.__categories['language_words']:
-            match_result = re.match(word['regex'], string)
-            if match_result != None:
-                if match_result.start() == 0 and match_result.end() == len(string):
+            match_result = re.match(word['regex'], symbol)
+            if match_result is not None:
+                if match_result.start() == 0 and match_result.end() == len(symbol):
                     return word
         return None
